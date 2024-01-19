81385830ad10d911c64a5681a156ee5604ae3cbe
jr-cast <castanos@protonmail.com>
2024-01-18 21:35:55 -0700

    chore: update wine card + add  add-to-cart button

--- src/components/wineCard/WineCard.jsx
+++ src/components/wineCard/WineCard.jsx
@@ -1,9 +1,15 @@
 import { useState } from "react";
 import { GoHeart, GoHeartFill } from "react-icons/go";
+import {
+  PiShoppingCartSimpleLight,
+  PiShoppingCartSimpleFill,
+} from "react-icons/pi";
+
 import "../../styles/shop.scss";
 
 const WineCard = ({ wine }) => {
   const [saved, setSaved] = useState(false);
+  const [hover, sethover] = useState(false);
 
   return (
     // simple card can be improved
@@ -15,11 +21,32 @@ const WineCard = ({ wine }) => {
           <GoHeartFill size={24} className="heart" />
         )}
       </div>
+
       <img src={wine.imageLink} alt="wine-example" />
+
       <div className="info">
-        <h4>{wine.name}</h4>
+        <h3>{wine.name}</h3>
         <h4>{wine.location}</h4>
         <span style={{ fontWeight: "bold" }}>${wine.price} CAD</span>
+      </div>
+
+      <div className="add-container">
+        <input type="number" min="1" defaultValue="1" />
+        <button
+          onMouseEnter={() => {
+            sethover(true);
+          }}
+          onMouseLeave={() => {
+            sethover(false);
+          }}
+        >
+          {!hover ? (
+            <PiShoppingCartSimpleLight />
+          ) : (
+            <PiShoppingCartSimpleFill />
+          )}
+          Add
+        </button>
       </div>
     </div>
   );
