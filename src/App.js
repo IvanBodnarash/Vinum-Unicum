5e21c3ba8c26368bb729a0c5f5fe88f6221d1371
Ivan Bodnarash <bodnarash1999@gmail.com>
2024-06-20 19:23:09 -0600

    Changing main header + refactoring main page

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ import ShopItem from "./pages/shopItem/ShopItem";
 import Discover from "./pages/Discover/Discover";
 import AboutUs from "./pages/AboutUs/About Us";
 import Contact from "./pages/Contact/Contact";
-import "./App.scss";
+// import "./App.scss";
 
 function App() {
   return (
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
+import { NavLink } from 'react-router-dom';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faSearch } from '@fortawesome/free-solid-svg-icons';
 import { faCartPlus } from '@fortawesome/free-solid-svg-icons';
@@ -16,35 +16,46 @@ export default function Header() {
                 VINUM UNICUM
             </span>
             <nav className='header-nav'>
-                <ul className='header-nav-items'>
-                    <li className='nav-item'>
-                        <Link to='/'>Our Collection</Link>
-                    </li>
-                    <li className='nav-item'>
-                        <Link to='/shop'>Shop</Link>
-                    </li>
-                    <li className='nav-item'>
-                        <Link to='/discover'>Discover</Link>
-                    </li>
-                    <li className='nav-item'>
-                        <Link to='/aboutUs'>About Us</Link>
-                    </li>
-                    <li className='nav-item'>
-                        <Link to='/contact'>Contact</Link>
-                    </li>
-                    <li className='nav-item'>
-                        <span><FontAwesomeIcon icon={faSearch} /></span>
-                    </li>
-                    <li className='nav-item'>
-                        <span><FontAwesomeIcon icon={faCartPlus} /></span>
-                    </li>
-                    <li className='nav-item'>
+                <div className='header-nav-items'>
+                    <NavLink 
+                        to='/' 
+                        className={({ isActive }) => (isActive ? 'nav-item active' : 'nav-item')}
+                    >
+                        Our Collection
+                    </NavLink>
+                    <NavLink 
+                        to='/shop' 
+                        className={({ isActive }) => (isActive ? 'nav-item active' : 'nav-item')}
+                    >
+                        Shop
+                    </NavLink>
+                    <NavLink 
+                        to='/discover' 
+                        className={({ isActive }) => (isActive ? 'nav-item active' : 'nav-item')}
+                    >
+                        Discover
+                    </NavLink>
+                    <NavLink 
+                        to='/aboutUs' 
+                        className={({ isActive }) => (isActive ? 'nav-item active' : 'nav-item')}
+                    >
+                        About Us
+                    </NavLink>
+                    <NavLink 
+                        to='/contact' 
+                        className={({ isActive }) => (isActive ? 'nav-item active' : 'nav-item')}
+                    >
+                        Contact
+                    </NavLink>
+                    <span className='nav-item'><FontAwesomeIcon icon={faSearch} /></span>
+                    <span className='nav-item'><FontAwesomeIcon icon={faCartPlus} /></span>
+                    <div className='nav-item'>
                         <div className='nav-item-sign-in'>
                             <span>Sign In</span>
                             <span><FontAwesomeIcon icon={faUser} /></span>
                         </div>
-                    </li>
-                </ul>
+                    </div>
+                </div>
             </nav>
         </div>
     </header>
