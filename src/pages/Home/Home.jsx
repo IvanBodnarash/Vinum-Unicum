e0e4258956d1f45e3239b181ff4225cab0a82638
Ivan Bodnarash <bodnarash1999@gmail.com>
2024-06-21 17:50:51 -0600

    Working on maps svg + changing main page with header

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,22 +1,22 @@
 import React from "react";
 import "../../styles/homepage.scss";
-import "../../styles/main-style.scss"
+import "../../styles/main-style.scss";
 
 import WineShop from "../../components/productCarousel/WineShop";
 import brandWinesData from "./brands";
 
 export default function Home() {
   return (
-      <div className="">
-        <div className="home-page-wrapper">
-          <div className="page-banner">
-            <span>Our Collection</span>
-            <p>Only premium brands</p>
-          </div>
+    <div className="">
+      <div className="home-page-wrapper">
+        <div className="page-banner">
+          <a href="#wine-carousel">Our Collection</a>
+          <p>Only premium brands</p>
         </div>
-        <div className="wine-container-test">
-          <WineShop brandWinesData={brandWinesData} />
-        </div>
+      </div>
+      <div className="wine-container-test" id="wine-carousel">
+        <WineShop brandWinesData={brandWinesData} />
+      </div>
     </div>
-  )
+  );
 }
